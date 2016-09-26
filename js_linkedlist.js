5756a7651fbd63013d0160fe25fd209784c69793
radeshshiwlochan <radesh0430@gmail.com>
2016-09-25 22:21:53 -0400

    adds constructor for Node object and linkedList object

---
+++ js_linkedlist.js
@@ -0,0 +1,13 @@
+//constructor for making Node object
+function Node(data) {
+	this.data = data;
+	this.next = null;
+}
+
+//constructor for making the linked list 
+function SinglyList() {
+	this.listLength = 0;
+	this.head = null
+}
+
+
