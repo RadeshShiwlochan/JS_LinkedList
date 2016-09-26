97bc0ae8039759c5de8d19f9bde3dba19a7ada57
radeshshiwlochan <radesh0430@gmail.com>
2016-09-25 23:08:41 -0400

    adds code for remove Node

--- js_linkedlist.js
+++ js_linkedlist.js
@@ -45,6 +45,30 @@ SinglyList.prototype.searchNodeAt = function(position) {
 	
 };
 
+SinglyList.prototype.remove = function(position) {
+    var currentNode = this.head,
+        length = this._length,
+        count = 0,
+        message = {failure: 'Failure: non-existent node in this list.'},
+        beforeNodeToDelete = null,
+        nodeToDelete = null,
+        deletedNode = null;
+ 
+    // 1st use-case: an invalid position
+    if (position < 0 || position > length) {
+        throw new Error(message.failure);
+    }
+ 
+    // 2nd use-case: the first node is removed
+    if (position === 1) {
+        this.head = currentNode.next;
+        deletedNode = currentNode;
+        currentNode = null;
+        this._length--;
+         
+        return deletedNode;
+    }
+
 
 
 
